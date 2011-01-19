94ac715777f8a82d1a420ade17b37620be290c5b
opatut <opatutlol@aol.com>
2011-01-19 22:10:07 +0100

    While trying to make circular waves, something strange happened. Use previous commit for normal waves. ;)

--- data/underwater.glsl
+++ data/underwater.glsl
@@ -2,9 +2,51 @@ uniform sampler2D tex;
 uniform float total_time;
 float speed = 0.5;
 float fac = 0.01;
+
+// advanced settings
+float amplitude = 0.01;	// factor of final displacement
+float velocity = 10;	// screens per second
+float wavelength = 0.1;		// screens
+
+// calculated values
+float peroid_time = wavelength / velocity;
+float frequency = 1.0 / peroid_time;
+
 float pi = 2.0 * asin(1.0);
 
-float rand(float c){
+float floatToRad(float f) {
+	//f = clamp(f, 0.0, 1.0);
+	return pi * 2 * f;
+}
+
+float timeForDistance(float distance) {
+	return distance / velocity;
+}
+float phaseForDistance(float distance) { // 0..1
+	return mod(distance, wavelength) / wavelength;
+}
+
+float displacementForPhase(float phase) { // -1 .. 1
+	return sin(floatToRad(phase));
+}
+
+float displacementForDistance(float distance) {
+	return displacementForPhase(phaseForDistance(distance));
+}
+float GetPhase(vec2 point1, vec2 point2, float time) {
+	float distance = sqrt( pow(point1.x - point2.x,2) + pow(point1.y - point2.y, 2) );
+	if(timeForDistance(distance) > time) {
+		return 0.0;
+	} else {
+		return displacementForDistance(distance) * sin(time);
+	}
+}
+
+/*vec2 transformCoord(vec2 orig) {
+
+}*/
+
+/*float rand(float c){
     return sin(c);
 }
 
@@ -21,9 +63,14 @@ vec2 transformCoord(vec2 orig) {
 	vec2 new_orig = vec2(r * sin( cos(co.x * 10 - 1.0) + total_time),
 						 r * sin( cos(co.y * 10 + 1.0) + total_time));
 	return orig + fac * new_orig;
+}*/
+
+vec4 transformColor(vec4 c, vec2 p) {
+	return c * GetPhase(p, vec2(0,0), total_time);
 }
 
 void main() {
-    vec2 coord = transformCoord(gl_TexCoord[0].st);
-    gl_FragColor = texture2D(tex, coord);
+	//vec2 coord = transformCoord(gl_TexCoord[0].st);
+	vec2 coord = gl_TexCoord[0].st;
+	gl_FragColor = transformColor(texture2D(tex, coord), coord);
 }
